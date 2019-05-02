c313628aad9d3d6b4a82e1c1004624d2158b5329
PaulWebber <paulw@tehnerdz.com>
2019-05-02 10:54:30 -0600

    footer images

---
+++ create_git.sh
@@ -0,0 +1,6 @@
+git init
+touch README.md
+git add .
+git remote add origin http://192.168.0.168:3000/pantgogs/dontbemepodcast_site.git
+git commit -m "first"
+git push -u origin master
---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+echo What comment should I add?
+read comment
+git commit -m \"$comment\"
+git push origin master
+echo pantgogs
+echo dcMbNAiNUcX2m79m6g8tWevkD99MpzQ6svuH6CX0
