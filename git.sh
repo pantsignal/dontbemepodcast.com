2c755764b6e99b2029abb5263bef505f93fdd86a
PaulWebber <paulw@tehnerdz.com>
2019-05-05 16:21:11 -0600

    deleted

--- git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .
-echo What comment should I add?
-read comment
-git commit -m \"$comment\"
-git push origin master
-echo pantgogs
-echo dcMbNAiNUcX2m79m6g8tWevkD99MpzQ6svuH6CX0
